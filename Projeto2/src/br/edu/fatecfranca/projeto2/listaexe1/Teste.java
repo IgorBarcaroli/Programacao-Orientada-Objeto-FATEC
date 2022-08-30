3cbecc8d695d91f2f5a2f6d71644b944bc1da5d1
Igor Barcaroli <89078062+IgorBarcaroli@users.noreply.github.com>
2022-08-30 12:43:04 -0300

    30/08

---
+++ Projeto2/src/br/edu/fatecfranca/projeto2/listaexe1/Teste.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package br.edu.fatecfranca.projeto2.listaexe1;
+
+/**
+ *
+ * @author 1090482111027
+ */
+public class Teste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Cliente obj1 = new Cliente();
+        obj1.setNome("José Oliveira");
+        obj1.setNroConta("144057-8");
+        obj1.setNroAgencia("1477-5");
+        obj1.setSaldo(500);
+        obj1.mostrar();
+        
+        
+    }
+    
+}
