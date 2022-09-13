cd427264097bf80da02c63598e5bf093b09eaba3
Igor Barcaroli <89078062+IgorBarcaroli@users.noreply.github.com>
2022-09-13 11:10:21 -0300

    (13/09) - Projeto 4

---
+++ Projeto4/src/main/java/br/edu/fatecfranca/projeto4/exe0/FIlho.java
@@ -0,0 +1,15 @@
+
+package br.edu.fatecfranca.projeto4.exe0;
+
+public class FIlho extends Pai {
+    private String namorada;
+    private boolean moraComPais;
+    
+    public void vamosBrincar() {
+        this.cargaGenetica = "Cabelos brancos e ansiedade";
+        this.moraComPais = true;
+        this.imoveis = "Nenhum, ele é pobre também";
+        this.namorada = "não tenho";
+        this.sobrenome = "Oliveira";
+    }
+}
---
+++ Projeto4/src/main/java/br/edu/fatecfranca/projeto4/exe0/Pai.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.fatecfranca.projeto4.exe0;
+
+/**
+ *
+ * @author 1090482111027
+ */
+public class Pai {
+    /*
+        Todo pai tem variávis com visibilidade
+        protegida (protected)
+        protected -> são públicas para as classes filhas
+        protected -> são privadas para classes que não fazem parte da hierarquia
+    */
+    
+    protected String nome;
+    protected String sobrenome;
+    protected String imoveis;
+    protected String cargaGenetica;
+}
